6c349d004cd5dc51a540fe8dae9a3eafccb91336
Daniel Sandiego <dnlsandiego@gmail.com>
2017-05-19 15:58:10 -0500

    refactor index.js

--- index.js
+++ index.js
@@ -3,6 +3,27 @@
 const childProcess = require('child_process');
 const prettyjson = require('prettyjson');
 
-const path = childProcess.execSync('echo $PATH').toString().trim().split(':');
+function getPath() {
+    const path = childProcess.execSync('echo $PATH').toString().trim();
 
-console.log(prettyjson.render(path));
+    if (!path) {
+        throw new Error('Unable to access `$PATH`');
+    }
+
+    return path.split(':');
+}
+
+function outputPrettyPath() {
+    const prettyPath = prettyjson.render({ PATH: getPath() });
+    console.log(prettyPath);
+}
+
+if (require.main === module) {
+    outputPrettyPath();
+}
+
+// export funcs so functionality can be `require`d by other modules.
+module.exports = {
+    getPath: getPath,
+    outputPrettyPath: outputPrettyPath
+};
